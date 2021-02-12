6848c9c64acad35ff287cc15ed526177f0331da1
SymplyTheHuman <43094600+EvgeniiMendeleev@users.noreply.github.com>
2021-02-12 14:04:03 +0300

    Initial commit

---
+++ Assets/SwitchToSceneExample.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Valve.VR.InteractionSystem;
+
+public class SwitchToSceneExample : MonoBehaviour
+{
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.transform.parent?.transform.parent?.GetComponent<Player>())
+        {
+            SceneManager.LoadScene("corridor_33");
+        }
+    }
+}
