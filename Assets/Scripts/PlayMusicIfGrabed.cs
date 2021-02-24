a87368ae80461d1a4856f38b798b4573bcfabc60
SymplyTheHuman <43094600+EvgeniiMendeleev@users.noreply.github.com>
2021-02-24 10:15:58 +0300

    Guider was added

---
+++ Assets/Scripts/PlayMusicIfGrabed.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class PlayMusicIfGrabed : MonoBehaviour
+{
+    [SerializeField] private AudioClip voiceOfGuider;
+    private Interactable interactable;
+    private AudioSource soundSource;
+
+    void Start()
+    {
+        interactable = GetComponent<Interactable>();
+        soundSource = gameObject.AddComponent<AudioSource>();
+        soundSource.clip = voiceOfGuider;
+    }
+
+    private void HandHoverUpdate(Hand hand)
+    {
+        GrabTypes grabTypes = hand.GetGrabStarting();
+        if (interactable.attachedToHand == null && grabTypes != GrabTypes.None)
+        {
+            if (!soundSource.isPlaying)
+            {
+                soundSource.Play();
+            }
+        }
+    }
+}
